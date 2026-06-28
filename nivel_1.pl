f5d0021e3ec453c464ebbcbea5ef09c7f6879c65
WolfDev616 <andreasamb_@hotmail.com>
2026-06-28 20:11:52 -0300

    Alterações no código nivel_1.pl adicionando novas funções (irma_espoir e irmao_espoir) e alterando as funções filhos_homens e filhas_mulheres

--- nivel_1.pl
+++ nivel_1.pl
@@ -1,13 +1,13 @@
-homem(grand-pere1).
-homem(grand-pere2).
+homem('grand-pere1').
+homem('grand-pere2').
 homem(aholou).
 homem(espoir).
 homem(ernest).
 homem(alfred).
 homem(stephane).
 
-mulher(grand-mere1).
-mulher(grand-mere2).
+mulher('grand-mere1').
+mulher('grand-mere2').
 mulher(charlotte).
 mulher(carmelle).
 mulher(marie).
@@ -21,10 +21,10 @@ irmao_espoir(alfred).
 irmao_espoir(ernest).
 irmao_espoir(stephane).
 
-gerou(grand-pere1, aholou).
-gerou(grand-mere1, aholou).
-gerou(grand-pere2, blandine).
-gerou(grand-mere2, blandine).
+gerou('grand-pere1', aholou).
+gerou('grand-mere1', aholou).
+gerou('grand-pere2', blandine).
+gerou('grand-mere2', blandine).
 gerou(aholou, charlotte).
 gerou(aholou, carmelle).
 gerou(aholou, marie).
@@ -42,5 +42,5 @@ gerou(blandine, stephane).
 
 filhos_homens(P, M, X):- ((homem(P), gerou(P, X)); (mulher(M), gerou(M, X))) , homem(X).
 filhas_mulheres(P, M, X):- ((homem(P), gerou(P, X)); (mulher(M), gerou(M, X))) , mulher(X).
-irmas_espoir(X, espoir):- (gerou(aholou, X); gerou(blandine, X)), irma_espoir(X).
-irmaos_espoir(X, espoir):- (gerou(aholou, X); gerou(blandine, X)), irmao_espoir(X).
+irmas(X, Y):- (gerou(Z, X), gerou(Z, Y)), mulher(X), X \= Y.
+irmaos(X, Y):- (gerou(Z, X), gerou(Z, Y)), homem(X), X \= Y.
